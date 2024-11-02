062be4d490a3f37097989e0221d34c6fcb75fe1f
chaem03 <hcm0309@naver.com>
2024-11-02 16:00:09 +0900

    Chore:초기 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,60 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { styled, ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global.js";
+import { theme } from "./styles/theme.js";
+import { Outlet } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Frame = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 420px;
+  background-color: white;
+`;
+
+const Content = styled.div`
+  flex-grow: 1;
+  min-height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Layout = () => {
+  return (
+    <Frame>
+      <Wrapper>
+        <Content>
+          <Outlet />
+        </Content>
+      </Wrapper>
+    </Frame>
+  );
+};
+
+function App() {
+  return (
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout />
+      </ThemeProvider>
+    </RecoilRoot>
+  );
+}
+
+export default App;

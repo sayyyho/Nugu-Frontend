c8c220123606c721c13faf2483e7d10942deb283
chaem03 <hcm0309@naver.com>
2024-11-03 20:02:22 +0900

    [Feature]- input 컴포넌트 제작

---
+++ src/components/InputComp/InputComp.jsx
@@ -0,0 +1,12 @@
+import * as S from "./style";
+
+export const InputComp = () => {
+  return (
+    <label>
+      <S.textfield>
+        <div></div>
+        <S.InputContainer></S.InputContainer>
+      </S.textfield>
+    </label>
+  );
+};

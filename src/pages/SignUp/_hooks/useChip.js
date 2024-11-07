bce24a12cf1afbe5c40aa67956ba2fe4a47bac5a
sayyyho <323psh@naver.com>
2024-11-07 12:14:59 +0900

    [Fix] #4 - 키워드 칩 상태관리 로직 수정

--- src/pages/SignUp/_hooks/useChip.js
+++ src/pages/SignUp/_hooks/useChip.js
@@ -1,18 +1,36 @@
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { chipState } from "@atoms/chipState";
+import { signUpState } from "@atoms/signUpState";
+import { CHIP_DATA } from "@constants/chip";
 
 export const useChip = () => {
   const [selectedChip, setSelectedChip] = useRecoilState(chipState);
+  const setSignUpData = useSetRecoilState(signUpState);
+
   const selectedCount = selectedChip.filter((chip) => chip).length;
 
   const handleClickStatus = (index) => {
-    if (selectedCount >= 3 && !selectedChip[index]) {
+    const newSelectedChip = [...selectedChip];
+    newSelectedChip[index] = !newSelectedChip[index];
+
+    if (newSelectedChip.filter((chip) => chip).length > 3) {
       return;
     }
 
-    const newSelectedChip = [...selectedChip];
-    newSelectedChip[index] = !newSelectedChip[index];
     setSelectedChip(newSelectedChip);
+
+    const selectedKeywords = newSelectedChip
+      .map((isSelected, idx) => (isSelected ? CHIP_DATA[idx] : null))
+      .filter((keyword) => keyword !== null);
+
+    if (selectedKeywords.length === 3) {
+      setSignUpData((prevData) => ({
+        ...prevData,
+        keyword1: selectedKeywords[0],
+        keyword2: selectedKeywords[1],
+        keyword3: selectedKeywords[2],
+      }));
+    }
   };
 
   return { selectedChip, handleClickStatus, selectedCount };
--- src/pages/SignUp/_hooks/useSignUp.js
+++ src/pages/SignUp/_hooks/useSignUp.js
@@ -3,19 +3,10 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { useNavigate } from "react-router-dom";
 
 import { signUpState } from "@atoms/signUpState";
-import { chipState } from "@atoms/chipState";
-import { CHIP_DATA } from "@constants/chip";
 
 export const useSignUp = (page) => {
   const navigate = useNavigate();
-
   const [signUpData, setSignUpData] = useRecoilState(signUpState);
-  const chipArray = useRecoilValue(chipState);
-  const chipResult = chipArray
-    .map((data, index) => (data === true ? CHIP_DATA[index] : null))
-    .filter((item) => item !== null);
-
-  console.log(signUpData);
 
   const handleInputChange = (field, value, index, fieldName, validateField) => {
     validateField(index, value, fieldName);
@@ -35,18 +26,6 @@ export const useSignUp = (page) => {
   };
 
   const handleSubmit = () => {
-    setSignUpData((prevData) => ({
-      ...prevData,
-      keyword1: chipResult[0],
-    }));
-    setSignUpData((prevData) => ({
-      ...prevData,
-      keyword2: chipResult[1],
-    }));
-    setSignUpData((prevData) => ({
-      ...prevData,
-      keyword3: chipResult[2],
-    }));
     console.log(signUpData);
     console.log("TODO: API 연결하기");
   };

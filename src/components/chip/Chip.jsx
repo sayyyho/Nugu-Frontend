43271dc7eab8df56aa306d7d987eff773fe842c1
sayyyho <323psh@naver.com>
2024-11-10 00:20:03 +0900

    [Chore] #7 - Github Cache Error 해결을 위한 임시 삭제

--- src/components/chip/Chip.jsx
+++
@@ -1,9 +0,0 @@
-import * as S from "./styled";
-
-export const Chip = ({ children, onClick, $backgroundColor }) => {
-  return (
-    <S.Chip $backgroundColor={$backgroundColor} onClick={onClick}>
-      {children}
-    </S.Chip>
-  );
-};
--- src/components/common/layout/Layout.jsx
+++
@@ -1,5 +0,0 @@
-import * as S from "./styled";
-
-export const Layout = ({ children, $backgroundColor }) => {
-  return <S.Layout $backgroundColor={$backgroundColor}>{children}</S.Layout>;
-};

c2b0ff18be9df5ae75c55bb6c1a85c8561e25abe
sayyyho <323psh@naver.com>
2024-11-14 04:03:00 +0000

    [Update] - upstream/main -> origin/main by the trigger

--- src/apis/nuguTest.js
+++ src/apis/nuguTest.js
@@ -3,13 +3,18 @@ import { instance } from "./instance";
 export const getNuguTestStatus = async () => {
   try {
     const response = await instance.get("/tests/status");
-    console.log(response);
-    if (response.data.hasTest) {
-      // TODO 수행한 인원 및 기타 작업
-      return true;
-    }
-    return false;
+    return response.data;
   } catch (err) {
+    console.error("테스트 상태 확인 중 오류가 발생했습니다:", err);
     throw err;
+  }
+};
+
+export const getNuguTestRanking = async () => {
+  try {
+    const response = await instance.get("/test-results/rankings");
+    return response.data;
+  } catch (err) {
+    console.log(err);
   }
 };

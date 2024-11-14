acf230f947b60e9a91027472e32f583b4346fa0e
sayyyho <323psh@naver.com>
2024-11-14 11:39:22 +0900

    [Fix] #41 - 헤더에 쿠키 직접 삽입

--- src/apis/nuguTest.js
+++ src/apis/nuguTest.js
@@ -1,15 +1,25 @@
 import { instance } from "./instance";
+import Cookies from "js-cookie";
 
 export const getNuguTestStatus = async () => {
   try {
-    const response = await instance.get("/tests/status");
-    console.log(response);
-    if (response.data.hasTest) {
-      // TODO 수행한 인원 및 기타 작업
+    const accessToken = Cookies.get("access_token");
+
+    if (!accessToken) {
+      console.error("토큰이 없습니다. 인증이 필요합니다.");
+    }
+
+    const response = await instance.get("/tests/status", {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    if (response) {
+      console.log("테스트 상태 확인:", response);
       return true;
     }
     return false;
   } catch (err) {
+    console.error("테스트 상태 확인 중 오류가 발생했습니다:", err);
     throw err;
   }
 };

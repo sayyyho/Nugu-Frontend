60b287c6eb82365d8e0fd03aebb9edf025632d91
chaem03 <hcm0309@naver.com>
2024-11-15 23:54:09 +0900

    [Feature]- 로그인, 회원가입 유효성 검사 구현

--- src/pages/signUp/_hooks/useSignUp.js
+++ src/pages/signUp/_hooks/useSignUp.js
@@ -1,12 +1,41 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 import { signUpState } from "@atoms/signUpState";
 
 export const useSignUp = (page) => {
   const navigate = useNavigate();
   const [signUpData, setSignUpData] = useRecoilState(signUpState);
+
+  const [errors, setErrors] = useState({});
+
+  const checkFieldValidity = (value, fieldName) => {
+    let error = "";
+    // 각 필드에 대한 유효성 검사
+    if (fieldName === "username" && (value.length === 0 || value.length > 20)) {
+      error = "아이디는 20자 이내로 작성해주세요.";
+    } else if (
+      fieldName === "password" &&
+      (value.length < 8 || value.length > 16)
+    ) {
+      error = "비밀번호는 8-16자로 작성해주세요.";
+    } else if (fieldName === "nickname" && value.length > 5) {
+      error = "닉네임은 5자 이내로 작성해주세요.";
+    } else if (fieldName === "mbti") {
+      value = value.trim(); // 공백 제거
+      // 영문 4자 검사
+      if (value && !/^[A-Za-z]{4}$/.test(value)) {
+        error = "MBTI는 영문 4자로 입력해주세요."; // 오류 메시지
+      }
+    } else if (fieldName === "organization" && value.length > 30) {
+      error = "소속은 30자 이내로 작성해주세요.";
+    } else if (fieldName === "intro" && value.length > 30) {
+      error = "한 줄 소개는 30자 이내로 작성해주세요.";
+    }
+
+    return error;
+  };
 
   const handleInputChange = (field, value, index, fieldName, validateField) => {
     validateField(index, value, fieldName);
@@ -17,6 +46,28 @@ export const useSignUp = (page) => {
     }));
   };
   const handleNextClick = (resetValidation, nextPath) => {
+    let valid = true;
+    const tempErrors = {}; // 오류를 임시로 저장할 객체
+
+    // 각 필드에 대해 유효성 검사
+    Object.keys(signUpData).forEach((field) => {
+      const value = signUpData[field];
+      const error = checkFieldValidity(value, field);
+      console.log(`Error for ${field}: ${error}`);
+
+      if (error !== "") {
+        valid = false;
+        tempErrors[field] = error;
+      } else {
+        delete tempErrors[field];
+      }
+    });
+    setErrors(tempErrors);
+    console.log(valid);
+    if (!valid) {
+      return;
+    }
+
     if (Number(page) === 3) {
       setSignUpData((prevData) => ({
         ...prevData,
@@ -26,6 +77,7 @@ export const useSignUp = (page) => {
     } else {
       navigate(nextPath);
     }
+
     resetValidation(Number(page));
   };
 
@@ -33,5 +85,6 @@ export const useSignUp = (page) => {
     console.log(signUpData);
     navigate("/signUp/create");
   };
-  return { handleInputChange, handleNextClick, handleSubmit };
+
+  return { handleInputChange, handleNextClick, handleSubmit, errors };
 };

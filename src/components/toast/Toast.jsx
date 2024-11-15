069b8d420c3261882b0d4d3f0fc536008f628269
sayyyho <323psh@naver.com>
2024-11-16 02:21:52 +0900

    [Fix] #98 - 토스트 UI 변경

--- src/components/toast/Toast.jsx
+++ src/components/toast/Toast.jsx
@@ -16,12 +16,22 @@ const slideDown = keyframes`
 export const ToastContainer = styled.div`
   position: fixed;
   top: 0;
+  width: 80%;
+  max-width: 300px;
+  display: flex;
+  justify-content: center;
   background-color: ${({ type, theme }) =>
+    type === "error" ? "#FEE9E9" : "#EAEFF8"};
+
+  box-shadow: ${({ type }) =>
+    type === "error"
+      ? "0px 0px 10px 0px rgba(255, 0, 0, 0.50)"
+      : "0px 0px 10px 0px rgba(55, 130, 237, 0.50)"};
+
+  color: ${({ type, theme }) =>
     type === "error" ? theme.colors.red200 : theme.colors.blue300};
-  color: white;
   padding: 1rem 2rem;
-  border-radius: 0.5rem;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 0 0 0.5rem 0.5rem;
   animation: ${slideDown} 0.5s ease;
   ${({ theme }) => theme.fonts.pretendardB4}
   z-index: 9999;
